7c7270dfa05a41822b210969cc381e48d2fb7f60
Swannhs <princeapurbask@gmail.com>
2021-08-18 06:14:22 -0700

    Added simple header with title

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import {View} from "react-native";
+import {Text, View, StyleSheet} from "react-native";
 
-const Header = () => {
+const Header = props => {
     return (
-        <View>
-
+        <View style={styles.header}>
+            <Text style={styles.headerText}>{props.title}</Text>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    header: {
+        width: '100%',
+        height: 60,
+        paddingTop: 36,
+        backgroundColor: '#2451d7',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    headerText: {
+        fontSize: 20,
+        color: '#e1b850'
+    }
+})
 
 export default Header;
