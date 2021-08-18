b740624dd6127473b26be8c5613609480bdc297f
Swannhs <princeapurbask@gmail.com>
2021-08-18 09:39:24 -0700

    Added simple flex style in app.js

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import React from "react";
-import {View} from "react-native";
-import Header from './src/components/Header';
+import {View, StyleSheet} from "react-native";
+import Header from './components/Header';
+import StartScreen from "./screens/StartScreen";
 
 function App(){
   return(
-      <View>
+      <View style={styles.screen}>
         <Header title='A simple game'/>
+          <StartScreen/>
       </View>
   )
 }
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1
+    }
+})
 
 export default App;
