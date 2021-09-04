c69edebb374a6f3eec6c93c282d43bcc1a47ee5e
Swannhs <princeapurbask@gmail.com>
2021-09-05 01:06:42 +0600

    Again start from screech

--- App.js
+++ App.js
@@ -1,13 +1,10 @@
 import React from "react";
 import {View, StyleSheet} from "react-native";
-import Header from './components/Header';
-import StartScreen from "./screens/StartScreen";
 
 function App(){
   return(
       <View style={styles.screen}>
-        <Header title='A simple game'/>
-          <StartScreen/>
+
       </View>
   )
 }
